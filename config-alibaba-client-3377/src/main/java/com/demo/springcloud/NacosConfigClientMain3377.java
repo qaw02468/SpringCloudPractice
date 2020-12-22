b8a16ca91eeccd9fbd0bd9db0d725c8b3f70d49f
qaw02468 <a890105311@gmail.com>
2020-12-22 15:00:33 +0800

    新增Nacos配置中心

---
+++ config-alibaba-client-3377/src/main/java/com/demo/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.demo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author - a89010531111@gmail.com
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
